7b0555a835403c1a5d8b595e2f27e43b181c0f66
SFERAS <rthyssen73@gmail.com>
2023-02-03 19:48:07 -0300

    3.2.2023 first look on schema

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Header.scss'
+import liya from '../src/images/liya.png'
 
 function Header() {
 	return (
 		<div className="nav">
 			<ul>
-				<li className="logo">LOGO</li>
+				<li className="logo"><img src={liya} alt="" /></li>
 				<li className="pricing">PRICING</li>
-				<li className="time">WORKING TIME</li>
+				<li className="time">OPEN TIME</li>
 				<li className="adress">ADRESS</li>
 				<li className="about">ABOUT</li>
 			</ul>
--- src/Mid-section.jsx
+++ src/Mid-section.jsx
@@ -1,14 +1,16 @@
 import React from "react"
 import './Mid-section.scss'
+import img1 from '../src/images/clothes.png'
+import img2 from '../src/images/img-two.png'
 
 function MidSection() {
 	return (
 		<div className="section">
-			<img src="" alt="" />
-			<img src="" alt="" />
-			<img src="" alt="" />
-			<img src="" alt="" />
-			<img src="" alt="" />
+			<img className="img1 img" src={img1} alt="" />
+			<img className="img2 img" src={img2} alt="" />
+			<img className="img3 img" src="" alt="" />
+			<img className="img4 img" src="" alt="" />
+			<img className="img5 img" src="" alt="" />
 		</div>
 	)
 }
