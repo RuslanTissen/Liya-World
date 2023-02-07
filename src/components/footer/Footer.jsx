183938138d6da07e7ebafbb6dc16623558021a6c
SFERAS <rthyssen73@gmail.com>
2023-02-07 15:55:12 -0300

    footer created

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import liya from '../../images/liya.png'
+
+export default function Footer() {
+	return (
+		<div className="footer">
+			<div className="footer-links">
+				<img src={liya} alt="" />
+				<a href=""></a>
+				<a href=""></a>
+				<a href=""></a>
+			</div>
+			<div className="footer-adress">
+
+			</div>
+			<div className="footer-contactes">
+				<p></p>
+				<p></p>
+				<p></p>
+				<p></p>
+			</div>
+		</div>
+	)
+}
