5dd5030fedf6f4e97778c9c12abbe24dc7ea35b2
SFERAS <rthyssen73@gmail.com>
2023-02-09 20:31:50 -0300

    9.2.2023 some burger staff done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import './Header.scss'
-import liya from '../../../src/images/liya.png'
-import { GiHamburgerMenu } from 'react-icons/gi';
+import liya from '../../../src/images/liya-white.png'
+// import { GiHamburgerMenu } from 'react-icons/gi';
 
 function Header() {
+	// const menuBtn = document.querySelector('.menu-btn');
+	// let menuOpen = false;
+	// menuBtn.addEventListener('click', () => {
+	// 	if (!menuOpen) {
+	// 		menuBtn.classList.add('open');
+	// 		menuOpen = true
+	// 	} else {
+	// 		menuBtn.classList.remove('open');
+	// 		menuOpen = false;
+	// 	}
+	// })
+
+	const handleClick = () => {
+		console.log('Hi Ruslan');
+	}
+
 	return (
 		<div className="nav">
 			<ul>
 				<li className="logo"><img src={liya} alt="" /></li>
-				<li className="pricing">PRICING</li>
+				<li className="pricing"><a href="#">PRICING</a> </li>
 				<li className="time">OPEN TIME</li>
 				<li className="adress">ADRESS</li>
 				<li className="about">ABOUT</li>
-				<GiHamburgerMenu/>
+				{/* <GiHamburgerMenu/> */}
+				<div onClick={handleClick} className="menu"><div className="menu-btn"></div></div>
 			</ul>
 		</div>
 	)
