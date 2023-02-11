833d0df1de0507dc5234b6f11801189943de6130
SFERAS <rthyssen73@gmail.com>
2023-02-11 14:30:06 -0300

    11.2.2023 dropDawn menu done, but still visable

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
 import liya from '../../../src/images/liya-white.png'
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 function Header() {
+	const [show, setShow] = useState(false)
+
 	// const menuBtn = document.querySelector('.menu-btn');
 	// let menuOpen = false;
 	// menuBtn.addEventListener('click', () => {
@@ -30,15 +32,22 @@ function Header() {
 
 	return (
 		<div className="nav">
-			<ul>
+			<ul className='ul-first'>
 				<li className="logo"><img src={liya} alt="image" /></li>
 				<li className="pricing"><a href="#">PRICING</a> </li>
 				<li className="time"><a href="#">OPEN TIME</a></li>
 				<li className="adress"><a href="#">ADRESS</a></li>
 				<li className="about"><a href="#">ABOUT</a></li>
-				<GiHamburgerMenu style={{fontSize:"1.5em"}}/>
+				<GiHamburgerMenu style={{ fontSize: "1.5em" }} onClick={() => setShow(!show)} />
 				{/* <div onClick={() => handleClick} className="menu"><div className="menu-btn"></div></div> */}
-			</ul>
+			</ul >
+			{show && <ul className='ul-second'>
+				<li className="pricing"><a href="#">PRICING</a> </li>
+				<li className="time"><a href="#">OPEN TIME</a></li>
+				<li className="adress"><a href="#">ADRESS</a></li>
+				<li className="about"><a href="#">ABOUT</a></li>
+			</ul>}
+
 
 		</div>
 	)
