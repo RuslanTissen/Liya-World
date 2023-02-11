47650e3bca219f6ec49604d58836222af8764212
SFERAS <rthyssen73@gmail.com>
2023-02-11 09:53:33 -0300

    11.2.2023 navbar + dropdown menu in progress

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.scss'
 import liya from '../../../src/images/liya-white.png'
-// import { GiHamburgerMenu } from 'react-icons/gi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 function Header() {
 	// const menuBtn = document.querySelector('.menu-btn');
@@ -16,21 +16,30 @@ function Header() {
 	// 	}
 	// })
 
-	const handleClick = () => {
-		console.log('Hi Ruslan');
-	}
+	// const handleClick = () => {
+	// 	const menuBtn = document.querySelector('.menu');
+	// 	let menuOpen = false;
+	// 	if (!menuOpen) {
+	// 		menuBtn.classList.add('open');
+	// 		menuOpen = true
+	// 	} else {
+	// 		menuBtn.classList.remove('open');
+	// 		menuOpen = false;
+	// 	}
+	// }
 
 	return (
 		<div className="nav">
 			<ul>
-				<li className="logo"><img src={liya} alt="" /></li>
+				<li className="logo"><img src={liya} alt="image" /></li>
 				<li className="pricing"><a href="#">PRICING</a> </li>
-				<li className="time">OPEN TIME</li>
-				<li className="adress">ADRESS</li>
-				<li className="about">ABOUT</li>
-				{/* <GiHamburgerMenu/> */}
-				<div onClick={handleClick} className="menu"><div className="menu-btn"></div></div>
+				<li className="time"><a href="#">OPEN TIME</a></li>
+				<li className="adress"><a href="#">ADRESS</a></li>
+				<li className="about"><a href="#">ABOUT</a></li>
+				<GiHamburgerMenu style={{fontSize:"1.5em"}}/>
+				{/* <div onClick={() => handleClick} className="menu"><div className="menu-btn"></div></div> */}
 			</ul>
+
 		</div>
 	)
 }
